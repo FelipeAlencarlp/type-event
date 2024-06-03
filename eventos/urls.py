cd1847edf76b0ec5ffd00abe7ae8ac1cf9b474ba
Felipe <felipealencar.0014@gmail.com>
2024-06-03 20:34:50 -0300

    gerenciar eventos, participar de um evento, gerar csv, listar participantes do evento

--- eventos/urls.py
+++ eventos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('novo_evento/', views.novo_evento, name="novo_evento"),
+    path('gerenciar_evento/', views.gerenciar_evento, name="gerenciar_evento"),
+    path('inscrever_evento/<int:evento_id>', views.inscrever_evento, name="inscrever_evento"),
+    path('participantes_evento/<int:evento_id>', views.participantes_evento, name="participantes_evento"),
+    path('gerar_csv/<int:evento_id>/', views.gerar_csv, name="gerar_csv"),
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render, redirect
+from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.conf import settings
 from .models import Evento
+import csv
+from secrets import token_urlsafe
+import os
 
 
-@login_required
+@login_required(login_url='/usuarios/login/')
 def novo_evento(request):
     if request.method == "GET":
         return render(request, 'novo_evento.html')
@@ -40,3 +45,68 @@ def novo_evento(request):
         
         messages.add_message(request, messages.SUCCESS, 'Evento cadastrado com sucesso')
         return redirect(reverse('novo_evento'))
+
+
+@login_required(login_url='/usuarios/login/')
+def gerenciar_evento(request):
+    nome = request.GET.get('nome')
+    eventos = Evento.objects.filter(criador=request.user)
+
+    if nome:
+        eventos = eventos.filter(nome__contais=nome)
+
+    return render(request, 'gerenciar_evento.html', {'eventos': eventos})
+
+
+@login_required(login_url='/usuarios/login/')
+def inscrever_evento(request, evento_id):
+    evento = get_object_or_404(Evento, id=evento_id)
+
+    if request.method == 'GET':
+        return render(request, 'inscrever_evento.html', {'evento': evento})
+
+    elif request.method == 'POST':
+        if request.user in evento.participantes.all():
+            messages.add_message(request, messages.ERROR, 'Você já está inscrito nesse evento.')
+            return redirect(reverse('inscrever_evento', kwargs={'id': evento_id}))
+        
+        evento.participantes.add(request.user)
+        evento.save()
+
+        messages.add_message(request, messages.SUCCESS, 'Inscrição realizada com sucesso.')
+        return redirect(reverse('inscrever_evento', kwargs={'id': evento_id}))
+
+
+@login_required(login_url='/usuarios/login/')
+def participantes_evento(request, evento_id):
+    evento = get_object_or_404(Evento, id=evento_id)
+
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+
+    if request.method == 'GET':
+        participantes = evento.participantes.all()[::3] # slice de lista do python
+        return render(request, 'participantes_evento.html', {'evento': evento,
+                                                             'participantes': participantes})
+
+
+@login_required(login_url='/usuarios/login/')
+def gerar_csv(request, evento_id):
+    evento = get_object_or_404(Evento, id=evento_id)
+
+    if not evento.criador == request.user:
+        raise Http404('Esse evento não é seu')
+    
+    participantes = evento.participantes.all()
+    
+    token = f'{token_urlsafe(6)}.csv'
+    path = os.path.join(settings.MEDIA_ROOT, token)
+
+    # abre o arquivo se existe, e escreve nele, se não existe cria 
+    with open(path, 'w') as arq:
+        writer = csv.writer(arq, delimiter=",")
+        for participante in participantes:
+            x = (participante.username, participante.email)
+            writer.writerow(x)
+
+    return redirect(f'/media/{token}')
